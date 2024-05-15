391132728add2ae5071e4d60b6928a7268600a94
Ale Rat <alexandra.rat87@gmail.com>
2024-05-16 02:01:39 +0300

    Added mapbox using EAS Build

--- HikeVentureApp/screens/MapScreen.js
+++ HikeVentureApp/screens/MapScreen.js
@@ -1,67 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableWithoutFeedback, View, Image, SafeAreaView, Platform, StatusBar } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import Mapbox from '@rnmapbox/maps';
 
-import MapView, { Marker } from 'react-native-maps';
+Mapbox.setAccessToken('<pk.eyJ1IjoiYWxlcmF0IiwiYSI6ImNsdjE4amdrdzA0ZXgybHFnbTd4cmQwbHEifQ.SKH-f-cKklc-I_WODvMe7g>');
+Mapbox.setTelemetryEnabled(false);
 
-// import MapboxGL from '@react-native-mapbox-gl/maps';
-// MapboxGL.setAccessToken('<pk.eyJ1IjoiYWxlcmF0IiwiYSI6ImNsdjE4amdrdzA0ZXgybHFnbTd4cmQwbHEifQ.SKH-f-cKklc-I_WODvMe7g>');
-
-function MapScreen(props) {
+export default function MapScreen() {
     return (
-      <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 45.59951058643169,
-          longitude: 24.736220694695465,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        mapType="terrain"
-      >
-        <Marker
-          coordinate={{ latitude: 45.59951058643169, longitude: 24.736220694695465 }}
-          title="Varful Moldoveanu"
-          description="destination"
+      <View style={styles.mapContainer}>
+      <Mapbox.MapView style={styles.map} zoomEnabled = {true} rotateEnabled = {true} styleURL = 'mapbox://styles/alerat/clw5i1tgf01gf01pc3cupbznz'>
+        <Mapbox.Camera 
+          zoomLevel={14}
+          centerCoordinate={[12.385263, 46.623314]}
+          pitch={60}
+          animationMode={'moveTo'}
+          animationDuration={6000}
         />
-      </MapView>
+        <Mapbox.PointAnnotation
+          id="marker"
+          coordinate={[12.385263, 46.623314]}
+        >
+        </Mapbox.PointAnnotation>
+      </Mapbox.MapView>
     </View>
-    // <View style={styles.container}>
-    // <MapboxGL.MapView style={styles.map}>
-    //   <MapboxGL.Camera
-    //     zoomLevel={10}
-    //     centerCoordinate={[45.600074997198945, 24.736948580013397]}
-    //   />
-    //   <MapboxGL.ShapeSource id="hikingTrails" url="https://www.openstreetmap.org/#map=15/45.3459/23.5597">
-    //     <MapboxGL.LineLayer id="hikingTrailsLayer" style={{ lineColor: 'brown', lineWidth: 3 }} />
-    //   </MapboxGL.ShapeSource>
-    // </MapboxGL.MapView>
-    // </View>
     );
 }
 const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  container: {
-    flex: 1,
+  mapContainer: {
+    flex: 1
   },
   map: {
-    flex: 1,
-  },
-  page: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  container: {
-    flex: 1,
-  },
-  map: {
-    flex: 1,
-  },
-})
-export default MapScreen;
+    flex: 1
+  }
+});
